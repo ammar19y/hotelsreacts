844da0b97f7962a06e7ba752a0f144b2b9dc1553
ammar19y <eng12.amar@gmail.com>
2024-10-15 19:59:57 +0300

    hotels

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import SubNavbar from './Components/Nav/subNav';
+import MainNav from './Components/Nav/mainNav';
+import Cards  from './Components/cards/cards';
 import './App.css';
+import Destenation from './Components/DestinationPage/Destenation';
+import ContactUs from'./Components/contactUsPage/contactUs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < >
+
+    <SubNavbar/>
+    <MainNav/>
+  <div className="hero-section">
+      <div className="text-center content">
+        <h1 style={{ fontFamily: 'Gloss And Bloom' }}>Memories for life</h1>
+        <p style={{ fontSize: '100px' }}>Let's Explore the world</p>
+      </div>
     </div>
+    <div className="pageTitle">
+      <h1 className="pageHeader">Welcome to Our Journey</h1>
+      <div>Discover Now</div>
+    </div>
+   
+    </>
   );
 }
 
